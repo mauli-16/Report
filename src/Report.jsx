f3b0498d92e6aacfcfeca5dd42d947fddf342f97
Mauli Saxena <maulisaxena.16@gmail.com>
2025-12-26 18:33:07 +0530

    added chart

--- src/Report.jsx
+++ src/Report.jsx
@@ -1,19 +1,66 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
-const Report = () => {
-    const performanceData=async()=>{
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+import { Bar, Doughnut,Line } from 'react-chartjs-2';
+const Report = () => { 
+    const [data,setData]=useState(null);
+    useEffect(()=>{
+      const performanceData=async()=>{
       const res = await fetch("http://localhost:3000/report");
-      const data=await res.json()
-      console.log(data);
+      const record=await res.json()
+      setData(record)
+
+      //console.log(data.studentName);
       
     }
     performanceData()
+  },[])
+    if (!data) {
+    return <div>Loading...</div>;
+  }
+  const goodscore="Excellent performance with strong control."
+  const mediumscore="Good performance with minor inaccuracies."
+  const lowscore="Needs improvement."
+  
+  console.log(data.overallScore);
+  
+
   return (
     
-    
-    <div>reportt
-      
+    <>
+    <div>Student report for {data.studentName}</div>
+    <div>{data.overallScore>=8 ? goodscore : data.overallScore>=6?mediumscore:lowscore}</div>
+    <div>{data.skills.fluency}</div>
+    <div>
+      <Bar data={{
+        labels:["Pronunciation", "Fluency", "Vocabulary", "Grammar"],
+        datasets:[
+          {
+            label:"Score",
+            data:Object.values(data.skills)
+          }
+        ]
+        }}></Bar>
     </div>
+    </>
   )
 }
 
